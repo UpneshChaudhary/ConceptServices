033a41a238ecca2ee1d97faf9959ac0c17223af8
Upnesh Chaudhary <121895772+UpneshChaudhary@users.noreply.github.com>
2024-11-22 13:13:58 +0530

    22nov

--- Conceptservices_web/settings.py
+++ Conceptservices_web/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-x03$_262k-&qbo^hhu=wh&s&esx!nio&e%m8xh=z3(&&8v+px+
 DEBUG = True
 
 ALLOWED_HOSTS = ['theconceptservices.com', 'www.theconceptservices.com', '44.195.176.252']
-
+#'theconceptservices.com', 'www.theconceptservices.com', '44.195.176.252'
 
 # Application definition
 
